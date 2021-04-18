cd1becceffe45948324defc7a3d8973bed77224e
Andy <andy@gettimesaved.com>
2021-04-18 02:27:23 -0400

    Add Ex Machina

--- mix.exs
+++ mix.exs
@@ -46,7 +46,9 @@ defmodule SimpleVote.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:ex_machina, "~> 2.7.0"},
+      {:mix_test_watch, "~> 1.0", only: :dev, runtime: false}
     ]
   end
 
