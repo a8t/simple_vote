650398dd976be55a9c59b11751181124570ac61d
Andy <andy@gettimesaved.com>
2021-07-04 14:08:11 -0400

    Add configure to skip tests

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,5 +1,6 @@
 # needs to be before ExUnit.start(): https://github.com/thoughtbot/ex_machina
 {:ok, _} = Application.ensure_all_started(:ex_machina)
 
+ExUnit.configure(exclude: :skip)
 ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(SimpleVote.Repo, :manual)
